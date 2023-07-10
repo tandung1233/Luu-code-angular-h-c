cd5449f60994afc63d063f437d8be727e71e1d65
tandung <nguyentandung9111@gmail.com>
2023-07-10 08:55:09 +0700

    first commit

---
+++ Du_an_angular_download/angular-14-full-course-main/src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2jbtI_i30STkCCGLxKQpOI-E3BD3j4wk",
+  authDomain: "angular14-course-7b7ac.firebaseapp.com",
+  projectId: "angular14-course-7b7ac",
+  storageBucket: "angular14-course-7b7ac.appspot.com",
+  messagingSenderId: "1060077589007",
+  appId: "1:1060077589007:web:8a91285e8f515b54dbc97a"
+};
